db893bbb1bcb9b24c040bee42bd5b9a39c5486b8
Дмитрий Шаплов <dshaplov@at-consulting.ru>
2020-07-10 15:15:15 +0300

    @feature security testing

---
+++ SpringTests/src/main/java/ru/shaplov/spring/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package ru.shaplov.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("admin.eee");
+    }
+}
--- SpringTests/src/main/java/ru/shaplov/spring/controller/TestController.java
+++ SpringTests/src/main/java/ru/shaplov/spring/controller/TestController.java
@@ -1,6 +1,9 @@
 package ru.shaplov.spring.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +20,23 @@ public class TestController {
 
     @GetMapping("/test")
     @ExecuteAspect
-    public Object testAspectMethod(HttpSession session, @RequestParam(required = false) String username) {
+    @Secured("ROLE_admin.eeE")
+    public UnitUser testAspectMethod(HttpSession session, @RequestParam(required = false) String username) {
         log.info("This is test");
         log.debug("{}", session.getAttribute("aspect"));
-        return "Test!";
+        UnitUser unitUser = null;
+        return unitUser;
     }
 
     @GetMapping("/test1")
     public Object testWOAspect(HttpSession session) {
         return "Test wo!";
     }
+}
+
+@Getter
+@Setter
+class UnitUser {
+    private String username;
+    private String description;
 }
