e143cfbc857e2c11bbedc2ce048770ba9ca6e713
Дмитрий Шаплов <dshaplov@at-consulting.ru>
2020-07-10 19:02:46 +0300

    добавил папку теста

---
+++ SpringTests/src/test/java/ru/shaplov/spring/aspect/TestedAspectTest.java
@@ -0,0 +1,12 @@
+package ru.shaplov.spring.aspect;
+
+import org.junit.Test;
+import org.modelmapper.ModelMapper;
+
+public class TestedAspectTest {
+
+    @Test
+    public void test() {
+//        System.out.println(new ModelMapper().map(null, String.class));
+    }
+}
